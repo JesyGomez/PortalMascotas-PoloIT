05181b57b487b76d0b0db99b9e13c3ff9d33155c
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2025-06-11 00:19:06 -0300

    Se agregan imagen y contenido sobre Nosotros a Inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
-import '../estilos/home.css'; // Asegurate de crear este archivo si no existe
+import '../estilos/home.css';
 
 const Home = () => {
   return (
-    <div className="home-hero">
-      <div className="home-overlay">
-        <h1>Portal Mascotas</h1>
-        <p>Encontrá, cuidá y adoptá con un clic</p>
-        <div className="home-buttons">
-          <button className="btn btn-primary">Ver mascotas</button>
-          <button className="btn btn-secondary">Quiero ayudar</button>
+    <div className="home">
+
+      <section className="home-hero"></section>
+
+      {/* BOTONES */}
+      <section className="home-acciones">
+        <button className="btn-voluntariado">VOLUNTARIADO</button>
+        <button className="btn-transito">QUIERO SER HOGAR DE TRÁNSITO</button>
+      </section>
+
+      {/* NOSOTROS */}
+      <section className="home-nosotros">
+        <div className="nosotros-wrapper">
+          <img src="/corazonAmor.png" alt="Adoptar es un acto de amor" className="nosotros-corazon" />
+
+          <div className="nosotros-caja">
+            <h2>NOSOTROS</h2>
+            <p>
+              Somos un equipo de personas impulsadas por el amor, el cariño y la empatía hacia las mascotas.
+              Nuestro objetivo es transformar vidas – tanto de los animales como de las personas – creando un puente
+              entre quienes buscan dar un hogar lleno de amor, dar segundas oportunidades, e inclusive tiempo y quienes
+              necesitan un hogar.
+              <br /><br />
+              Creemos en la adopción responsable, en el valor del tránsito como oportunidad y en la construcción de una
+              comunidad solidaria, donde cada acción cuenta para darles una segunda oportunidad a quienes más lo necesitan.
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
+      <section className="home-banner">
+        <p>
+          ¿Quieres convertirte en su segunda oportunidad?<br/>
+          <strong>¡Conócelos!</strong>
+        </p>
+      </section>
+
+
     </div>
   );
 };
