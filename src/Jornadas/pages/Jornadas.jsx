c8dba4b809e9a5c093872712f7e84878a90c86de
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2025-06-30 00:21:56 -0300

    Actualización de Jornadas

--- src/Jornadas/pages/Jornadas.jsx
+++ src/Jornadas/pages/Jornadas.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../../styles/jornadas.css';
+
 
 function Jornadas() {
   return (
-    <div>
-      Estas en la pagina de Jornadas
+    <div className="jornadas-container">
+      <img src="/mascotas4.png" alt="Mascotas asomadas" className="mascotas-img" />
+
+      <img src="/calendarCheck.png" alt="Calendario" className="calendar-icon" />
+
+      <p className="jornadas-text bold">
+        Próximamente estaremos trabajando para agregar información al respecto
+      </p>
+
+      <p className="jornadas-text">¡Una jornada de adopción estará pronto en camino!</p>
+
+      <p className="jornadas-text">
+        <span role="img" aria-label="gato">🐱</span> 
+        ¡No te pierdas de nuestros próximos eventos y campañas! 
+        <span role="img" aria-label="perro">🐶</span>
+      </p>
     </div>
   )
 }
