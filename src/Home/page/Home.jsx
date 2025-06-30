7cc1551f4e9f9237129ff4c094118a90a2863b48
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2025-06-29 22:26:53 -0300

    Estilos mejorados en sección inicio

--- src/Home/page/Home.jsx
+++ src/Home/page/Home.jsx
@@ -28,12 +28,18 @@ export const Home = () => {
           <img src="/corazonAmor.png" alt="Adoptar es un acto de amor" className="nosotros-corazon" />
           <div className="nosotros-caja">
             <h2>NOSOTROS</h2>
-            <p>
-              Somos un equipo de personas impulsadas por el amor, el cariño y la
-              empatía hacia las mascotas...
-            </p>
+            <p>Somos un equipo de personas impulsadas por el amor, el cariño y la empatía hacia las mascotas. Nuestro objetivo es transformar vidas — tanto de los animales como de las personas — creando un puente entre quienes buscan dar un hoga lleno de amor, dar segundas oportunidades, e inclusive tiempo y quienes necesitan un hogar.</p>
+              
+            <p>Creemos en la adopción responsable, en el valor del tránsito como oportunidad y en la construcción de una comunidad solidaria, donde cada acción cuenta para darles una segunda oportunidad a quienes más lo necesitan.</p>
           </div>
         </div>
+      </section>
+
+      <section className="home-banner">
+        <p>
+          ¿Querés convertirte en su segunda oportunidad?<br />
+          <strong>¡Conocelos!</strong>
+        </p>
       </section>
 
       <div className="container-fluid">
