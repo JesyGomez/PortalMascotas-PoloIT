3682c568a1108367ad4682d24584b33167fa0e38
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2025-06-30 11:57:26 -0300

    Actualización de Donaciones

--- src/Donaciones/pages/Donaciones.jsx
+++ src/Donaciones/pages/Donaciones.jsx
@@ -1,8 +1,24 @@
+import React from 'react'
+import '../../styles/donaciones.css';
 
 export const Donaciones = ()=> {
   return (
-    <div>
-      Estas en la pagina de Donaciones
+    <div className="donaciones-container">
+      <div className="donaciones-img-section">
+        <img src="/perritoCorazon.png" alt="Perrito con manos en forma de corazón" />
+      </div>
+
+      <div className="donaciones-info">
+        <p className="donaciones-text">
+          Tus donaciones nos ayudan a proporcionarle alimento, atención médica y refugio a todas las mascotas
+        </p>
+
+        <img src="/mercadoPago.png" alt="Logo de Mercado Pago" className="mercado-logo" />
+        <p className="donaciones-alias">ALIAS: PATITASENCASA.MP</p>
+        <hr className="donaciones-linea" />
+
+        <button className="donaciones-btn">DONÁ AHORA</button>
+      </div>
     </div>
   )
 }
